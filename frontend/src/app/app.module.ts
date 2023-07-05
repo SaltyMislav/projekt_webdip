fada77b389b3d4273050eac606ff650c5345f032
Mislav <znidarec01@gmail.com>
2023-07-05 18:42:04 +0200

    preimenovanje komponenti

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,19 +12,13 @@ import { PoduzecaComponent } from './modules/poduzeca/poduzeca.component';
 import { RadniZadatakComponent } from './modules/radni-zadatak/radni-zadatak.component';
 import { NatjecajComponent } from './modules/natjecaj/natjecaj.component';
 import { DolazakNaPosaoComponent } from './modules/dolazak-na-posao/dolazak-na-posao.component';
-import { AppHeaderComponent } from './app-header.component';
 import { AppFooterComponent } from './app-footer.component';
+import { AppHeaderComponent } from './app-header.component';
 import { AppMenuComponent } from './app-menu.component';
-import { AppMenuComponentComponent } from './app.menu.component.component';
-import { AppFooterComponentComponent } from './app.footer.component.component';
-import { AppHeaderComponentComponent } from './app.header.component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderComponent,
-    AppFooterComponent,
-    AppMenuComponent,
     RegistracijaComponent,
     PrijavaComponent,
     KorisniciComponent,
@@ -32,9 +26,9 @@ import { AppHeaderComponentComponent } from './app.header.component.component';
     RadniZadatakComponent,
     NatjecajComponent,
     DolazakNaPosaoComponent,
-    AppMenuComponentComponent,
-    AppFooterComponentComponent,
-    AppHeaderComponentComponent
+    AppFooterComponent,
+    AppHeaderComponent,
+    AppMenuComponent
   ],
   imports: [
     BrowserModule,
