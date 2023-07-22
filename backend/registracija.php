bbde01d86c00077383e9a91328da31ae25341665
Mislav <znidarec01@gmail.com>
2023-07-23 00:23:12 +0200

    korisnik dodavanje

--- backend/registracija.php
+++ backend/registracija.php
@@ -4,95 +4,114 @@ require 'connection.php';
 
 $postData = file_get_contents("php://input");
 
-if (isset($postData) && !empty($postData))
-{
+if (isset($postData) && !empty($postData)) {
     $result = json_decode($postData);
 
-    if (validate_ime($result) && validate_prezime($result) && validate_userName($result) || validate_email($result))
-    {  
-        echo json_encode(['data' => 'Success']);
+    $ime = validate_ime($result);
+    $prezime = validate_prezime($result);
+    $userName = validate_userName($result);
+    $email = validate_email($result);
+    $password = $result->data->password;
+
+
+    //check if user already exists
+    provjeraKorisnika($userName, $con);
+
+    //check if passwords match
+    if ($result->data->password != $result->data->confirmedPassword) {
+        trigger_error("Lozinke se ne podudaraju", E_USER_ERROR);
+    }
+
+    //validate password with regex
+    if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/", $password)) {
+        trigger_error("Lozinka mora imati najmanje 8 znakova, jedno veliko slovo i jedan broj", E_USER_ERROR);
+    } else {
+        $passwordHashed = password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    $sql = "INSERT INTO korisnik (Ime, Prezime, KorisnickoIme, Email, Password, Lozinka_Upis, DatumRegistracije, UlogaKorisnikaID) VALUES ('$ime', '$prezime', '$userName', '$email', '$passwordHashed', '$password', NOW(), 1)";
+    //insert data into database
+    if (mysqli_query($con, $sql)) {
+        http_response_code(201);
+    } else {
+        http_response_code(422);
     }
 }
 
 
-function validate_ime($result) {
-    if (isset($result->data->ime) && !empty($result->data->ime))
-    {
+function validate_ime($result)
+{
+    if (isset($result->data->ime) && !empty($result->data->ime)) {
         $ime = trim($result->data->ime);
-        $ime = filter_var($ime, 513);
-
-        if (strlen($ime) < 3 || strlen($ime) > 20)
-        {
+        $ime = strip_tags($ime);
+        $ime = htmlspecialchars($ime);
+        if (strlen($ime) < 3 || strlen($ime) > 20) {
             trigger_error("Ime nema između 3 i 20 znakova", E_USER_ERROR);
-        }
-        else
-        {
+        } else {
             return $ime;
         }
-    }
-    else
-    {
+    } else {
         trigger_error("Ime nema između 3 i 20 znakova", E_USER_ERROR);
     }
 }
 
-function validate_prezime($result) {
-    if (isset($result->data->prezime) && !empty($result->data->prezime))
-    {
+function validate_prezime($result)
+{
+    if (isset($result->data->prezime) && !empty($result->data->prezime)) {
         $prezime = trim($result->data->prezime);
-        $prezime = filter_var($prezime, 513);
-        if (strlen($prezime) < 3 || strlen($prezime) > 20)
-        {
+        $prezime = strip_tags($prezime);
+        $prezime = htmlspecialchars($prezime);
+        if (strlen($prezime) < 3 || strlen($prezime) > 20) {
             trigger_error("Prezime nema između 3 i 20 znakova", E_USER_ERROR);
-        }
-        else
-        {
+        } else {
             return $prezime;
         }
-    }
-    else
-    {
+    } else {
         trigger_error("Prezime nema između 3 i 20 znakova", E_USER_ERROR);
     }
 }
 
-function validate_userName($result) {
-    if (isset($result->data->userName) && !empty($result->data->userName))
-    {
+function validate_userName($result)
+{
+    if (isset($result->data->userName) && !empty($result->data->userName)) {
         $userName = trim($result->data->userName);
-        $userName = filter_var($userName, 513);
-        if (strlen($userName) < 3 || strlen($userName) > 20)
-        {
+        $userName = strip_tags($userName);
+        $userName = htmlspecialchars($userName);
+        if (strlen($userName) < 3 || strlen($userName) > 20) {
             trigger_error("Korisničko ime nema između 3 i 20 znakova", E_USER_ERROR);
-        }
-        else
-        {
+        } else {
             return $userName;
         }
-    }
-    else
-    {
+    } else {
         trigger_error("Korisničko ime nema između 3 i 20 znakova", E_USER_ERROR);
     }
 }
 
 //function to validate email
-function validate_email($result) {
-    if (isset($result->data->email) && !empty($result->data->email))
-    {
+function validate_email($result)
+{
+    if (isset($result->data->email) && !empty($result->data->email)) {
         $email = trim($result->data->email);
-        $email = filter_var($email, 513);
-        if (!filter_var($email, 274))
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        if (!filter_var($email, 274)) //274 is FILTER_VALIDATE_EMAIL
         {
             trigger_error("Email nije ispravan", E_USER_ERROR);
-        }
-        else
-        {
+        } else {
             return $email;
         }
-    }
-    else
-    {
+    } else {
         trigger_error("Email nije ispravan", E_USER_ERROR);
     }
+}
+
+
+function provjeraKorisnika($userName, $con)
+{
+    $sqlProvjera = "SELECT * FROM korisnik WHERE KorisnickoIme = '$userName'";
+    $provjeraPodataka = mysqli_query($con, $sqlProvjera);
+    if (mysqli_num_rows($provjeraPodataka) > 0) {
+        trigger_error("Korisnik već postoji", E_USER_ERROR);
+    }
+
+    mysqli_close($con);
 }
