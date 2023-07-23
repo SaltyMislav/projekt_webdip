3632802641ffea026cb58fd9a4410bb8a5cd7a95
Mislav <znidarec01@gmail.com>
2023-07-23 02:06:54 +0200

    dodavanje slanja mail za potvrdu registracije

--- backend/registracija.php
+++ backend/registracija.php
@@ -12,6 +12,7 @@ if (isset($postData) && !empty($postData)) {
     $userName = validate_userName($result);
     $email = validate_email($result);
     $password = $result->data->password;
+    $confirmedPassword = $result->data->confirmedPassword;
 
 
     //check if user already exists
@@ -23,21 +24,48 @@ if (isset($postData) && !empty($postData)) {
     }
 
     //validate password with regex
-    if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/", $password)) {
-        trigger_error("Lozinka mora imati najmanje 8 znakova, jedno veliko slovo i jedan broj", E_USER_ERROR);
+    if (!preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/", $password)) {
+        trigger_error("Lozinka mora imati najmanje 8 znakova, jedno veliko slovo, jedan broj i jedan specijalni znak", E_USER_ERROR);
     } else {
         $passwordHashed = password_hash($password, PASSWORD_DEFAULT);
     }
 
-    $sql = "INSERT INTO korisnik (Ime, Prezime, KorisnickoIme, Email, Password, Lozinka_Upis, DatumRegistracije, UlogaKorisnikaID) VALUES ('$ime', '$prezime', '$userName', '$email', '$passwordHashed', '$password', NOW(), 1)";
+    $token = bin2hex(openssl_random_pseudo_bytes(20));
+
+    //token expiry time set to 10 hours
+    $expiry = 36000;
+    $istekAktivacije = time() + $expiry; //86400 = 1 dan
+
+    $sql = "INSERT INTO korisnik (Ime, Prezime, KorisnickoIme, Email, Password, Lozinka_Upis, DatumRegistracije, Token, IstekTokena, UlogaKorisnikaID) VALUES ('$ime', '$prezime', '$userName', '$email', '$passwordHashed', '$password', NOW(), '$token', '$istekAktivacije', 1)";
     //insert data into database
     if (mysqli_query($con, $sql)) {
+        slanjeMaila($email, $token);
         http_response_code(201);
     } else {
         http_response_code(422);
     }
+    
+    mysqli_close($con);
 }
 
+
+function slanjeMaila($email, $token) {
+    $subject = "Registracija";
+    $message = "Uspješno ste se registrirali na stranicu! Molimo potvrdite svoj račun klikom na link: http://localhost/aktivacija/?email=$email&token=$token";
+    $headers = array(
+        'From' => 'mznidarec@foi.hr',
+        'Content-Type' => 'text/plain; charset=UTF-8' . '\r\n',
+    );
+    //send email
+    if (mail($email, $subject, $message, $headers))
+    {
+        echo "Uspješno ste se registrirali na stranicu!";
+    }
+    else
+    {
+        echo "Email nije poslan";
+    };
+}
 
 function validate_ime($result)
 {
@@ -112,6 +140,4 @@ function provjeraKorisnika($userName, $con)
     if (mysqli_num_rows($provjeraPodataka) > 0) {
         trigger_error("Korisnik već postoji", E_USER_ERROR);
     }
-
-    mysqli_close($con);
 }
